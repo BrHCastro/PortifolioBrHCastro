5ab22047f167dbfe7949846f037581a89f155d6b
Henrique de Castro <hendecastro@gmail.com>
2021-02-19 12:48:40 -0300

    Estrutura e estilo

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Meu portifólio. Aqui exibirei minhas experiências e habilidades.">
+    <meta property="og:title" content="Portifólio - Henrique de Castro">
+    <meta property="og:site_name" content="Meu portifólio">
+    <meta property="og:description" content="Portifólio - Henrique de Castro - Experiências e Habilidades">
+    <meta property="og:url" content="http://henrthudor.com.br">
+    <meta property="og:image" content="http://henrthudor.com.br/imagemtop.jpeg">
+    <meta property="og:image" content="image/jpeg">
+    <title>Portifólio - Henrique de Castro</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="parent">
+        <div class="sidebar">
+            <div class="sidebar-pos">
+                <div class="img-sidebar">
+                    <img src="img/foto.jpg" />
+                </div>
+                <!--img-->
+                <div class="menu">
+                    <a class="selected" href="#">Sobre</a>
+                    <a href="#">Experiência</a>
+                    <a href="#">Educação</a>
+                </div>
+                <!--menu-->
+            </div>
+            <!--sidebar-pos-->
+
+        </div>
+        <!--sidebar-->
+
+        <div class="content">
+            <section class="descricao">
+                <h2>Henrique de <span>Castro</span></h2>
+                <h3>Sorocaba - SP · <span>+55 (15) 99161-7077 · contato@henrthudor.com.br</span></h3>
+                <p>I am experienced in leveraging agile frameworks to provide a robust synopsis for high level
+                    overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the
+                    overall value proposition.</p>
+                <div class="icons-social">
+                    <a href="https://github.com/BrHCastro" target="_blank"><i class="fab fa-github"></i></a>
+                    <a href="https://www.linkedin.com/in/henrique-castro-a26504118" target="_blank"><i
+                            class="fab fa-linkedin-in"></i></i></a>
+                    <a href="https://www.facebook.com/henrique.decastro.520" target="_blank"><i
+                            class="fab fa-facebook-f"></i></a>
+                </div>
+                <!--icons-social-->
+            </section>
+        </div>
+        <!--content-->
+    </div>
+    <!--parent-->
+
+    <script src="js/jquery.js"></script>
+    <script src="https://kit.fontawesome.com/3e442d7cea.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
